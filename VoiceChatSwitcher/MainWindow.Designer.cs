26a961e5f2f176fb993c1df91dc3506a1b045fa0
Hawkeye1 <serega.nekrevich@gmail.com>
2017-07-30 07:27:02 +0300

    gui sketch without functional

---
+++ VoiceChatSwitcher/MainWindow.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace VoiceChatSwitcher
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.KeyTextBox = new System.Windows.Forms.TextBox();
+            this.HideButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // KeyTextBox
+            // 
+            this.KeyTextBox.Location = new System.Drawing.Point(13, 65);
+            this.KeyTextBox.Name = "KeyTextBox";
+            this.KeyTextBox.ReadOnly = true;
+            this.KeyTextBox.Size = new System.Drawing.Size(181, 20);
+            this.KeyTextBox.TabIndex = 0;
+            this.KeyTextBox.Text = "Выберети клавишу";
+            // 
+            // HideButton
+            // 
+            this.HideButton.Location = new System.Drawing.Point(297, 126);
+            this.HideButton.Name = "HideButton";
+            this.HideButton.Size = new System.Drawing.Size(75, 23);
+            this.HideButton.TabIndex = 1;
+            this.HideButton.Text = "Hide";
+            this.HideButton.UseVisualStyleBackColor = true;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 161);
+            this.Controls.Add(this.HideButton);
+            this.Controls.Add(this.KeyTextBox);
+            this.MaximumSize = new System.Drawing.Size(400, 200);
+            this.MinimumSize = new System.Drawing.Size(400, 200);
+            this.Name = "MainWindow";
+            this.Text = "VoiceChatSwitcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox KeyTextBox;
+        private System.Windows.Forms.Button HideButton;
+    }
+}
+
---
+++ VoiceChatSwitcher/MainWindow.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VoiceChatSwitcher
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            KeyTextBox.KeyDown += KeyTextBox_KeyDown;
+            KeyTextBox.MouseDown += KeyTextBox_MouseDown;
+            this.MouseDown += MainWindow_MouseDown;
+        }
+
+        private void KeyTextBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            //KeyTextBox.HideSelection = false;
+        }
+
+        private void MainWindow_MouseDown(object sender, MouseEventArgs e)
+        {
+            HideButton.Focus();
+        }
+
+        private void KeyTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            InitKey(e.KeyData);
+
+        }
+
+        private void InitKey(Keys key)
+        {
+            KeyTextBox.Text = key.ToString();
+        }
+    }
+}
